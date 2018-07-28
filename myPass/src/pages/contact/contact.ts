945dd6bf33f6ff616ad16fab116483eb2d82ef21
Sean Thornton <seanthornton@Seans-MacBook-Pro-2.local>
2018-07-28 18:05:52 -0500

    added a few contacts

--- myPass/src/pages/contact/contact.ts
+++ myPass/src/pages/contact/contact.ts
@@ -7,6 +7,27 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+	contacts = [
+		"Austin Resource Center for the Homeless",
+		"Central Health",
+		"Austin Community Court",
+	]
+
+	contact_data = {
+		"Austin Resource Center for the Homeless": {
+			Phone: "(512) 305-4100",
+			Address: "500 E 7th St, Austin, TX 78701"
+		},
+		"Central Health": {
+			Phone: "(512) 978-8130",
+			Address: "6633 E Hwy 290 #310, Austin, TX 78723"
+		},
+		"Austin Community Court": {
+			Phone: "(512) 974-4879",
+			Address: "719 E 6th St, Austin, TX 78701"
+		}
+	}
+
   constructor(public navCtrl: NavController) {
 
   }
