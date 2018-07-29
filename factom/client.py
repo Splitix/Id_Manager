cd416518311c430b8e8844616d914285bd8b2914
Reid Cumbest <reid@Reids-MacBook-Pro.local>
2018-07-28 19:03:29 -0500

    Add reveal to factom/client.py

--- factom/client.py
+++ factom/client.py
@@ -8,12 +8,28 @@ URL = 'http://localhost:8089/v2'
 HEADERS = {'content-type' : 'text/plain'}
 
 def commit_chain(ec, content, state):
+    commit,reveal = _get_commit_chain_message(ec, content, state)
     data = {
         "id": 0,
         "jsonrpc": "2.0",
         "method": "commit-chain",
         "params": {
-            "message": _get_commit_chain_message(ec, content, state)
+            "message": commit
+            }
+        }
+    r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
+    if DEBUG:
+        print(r.text)
+    _reveal_chain(reveal)
+    return r.text
+
+def _reveal_chain(reveal):
+    data = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": "reveal-chain",
+        "params": {
+            "entry": reveal
             }
         }
     r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
@@ -41,29 +57,30 @@ def _get_commit_chain_message(ec, content, state):
     r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
     if DEBUG:
         print(r.text)
-    return r.text['result']['commit']['params']['message']
-
-def commit_chain(ec, content, state):
-    data = {
-        "id": 0,
-        "jsonrpc": "2.0",
-        "method": "commit-chain",
-        "params": {
-            "message": get_commit_chain_message(ec, content, state)
-            }
-        }
-    r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
-    if DEBUG:
-        print(r.text)
-    return r.text
+    return r.text['result']['commit']['params']['message'], r.text['result']['reveal']['params']['entry']
 
 def commit_entry(ec, chainid, content, state):
+    commit,reveal = _get_commit_entry_message(ec, chainid, content, state)
     data = {
         "id": 0,
         "jsonrpc": "2.0",
         "method": "commit-entry",
         "params": {
-            "message": _get_commit_entry_message(ec, chainid, content, state)
+            "message": commit
+            }
+        }
+    r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
+    if DEBUG:
+        print(r.text)
+    _reveal_entry(reveal)
+
+def _reveal_entry(reveal):
+    data = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": "reveal-entry",
+        "params": {
+            "entry": reveal
             }
         }
     r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
@@ -90,7 +107,7 @@ def _get_commit_entry_message(ec, chainid, content, state):
     r = requests.request("POST", URL, data=json.dumps(data), headers=HEADERS)
     if DEBUG:
         print(r.text)
-    return r.text['result']['commit']['params']['message']
+    return r.text['result']['commit']['params']['message'], r.text['result']['reveal']['params']['entry']
 
 def get_entry(ec, hash):
     data = {
