a9cbbcb731027ced638c76f0aa56d75ced92ae88
Josh Galindo <galindo@Joshs-MacBook-Pro.local>
2018-07-29 09:50:44 -0500

    Updated card

--- myPass/src/pages/home/home.ts
+++ myPass/src/pages/home/home.ts
@@ -21,9 +21,10 @@ export class HomePage {
 
   data = [
     {views: '9 Views',title: 'Identification Card', front:'idCard.jpg', back:'idCard_back.png', expires:'01-01-2020', verified:'Sean Thornton', color:'color1.png', exp_status:'exp_bar_good.png'},
-    {views: '2 Views',title: 'Birth Certificate', front:'idCard.jpg', back:'idCard_back.png', expires:'01-01-2018', verified:'Joe Smith', color:'color2.png', exp_status:'exp_bar_bad.png'},
-    {views: '7 Views',title: 'MAP Card', front:'map_card.jpg', back:'idCard_back.png', expires:'09-01-2020', verified:'John Doe', color:'color3.png', exp_status:'exp_bar_good.png'},
-    {views: '1 Views',title: 'Disability Id', front:'idCard.jpg', back:'idCard_back.png', expires:'09-01-2018', verified:'Chuck Norris', color:'color4.png', exp_status:'exp_bar_soon.png'},
+    {views: '2 Views',title: 'Birth Certificate', front:'idCard.jpg', back:'idCard_back.png', expires:'00-00-0000', verified:'Joe Smith', color:'color2.png', exp_status:'exp_bar_good.png'},
+    {views: '2 Views',title: 'Passport', front:'idCard.jpg', back:'idCard_back.png', expires:'01-01-2018', verified:'Joe Smith', color:'color3.png', exp_status:'exp_bar_bad.png'},
+    {views: '7 Views',title: 'MAP Card', front:'map_card.jpg', back:'idCard_back.png', expires:'09-01-2020', verified:'John Doe', color:'color4.png', exp_status:'exp_bar_good.png'},
+    {views: '1 Views',title: 'Disability Id', front:'idCard.jpg', back:'idCard_back.png', expires:'09-01-2018', verified:'Chuck Norris', color:'color1.png', exp_status:'exp_bar_soon.png'},
   ];
 
   goToDetails(data):void {
